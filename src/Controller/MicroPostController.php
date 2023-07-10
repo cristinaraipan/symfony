c87f6ba24ba89c3eed91ff209ee44c705b3fe7b0
Cristina Nicol Raipán Campos <cristina.raipan@globalconexus.com>
2023-07-10 16:01:00 -0400

    se agrega redireccion y un comentario al agregar datos

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -5,27 +5,17 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
     public function index(MicroPostRepository $posts): Response
-    {
-        //dd($posts->findAll());//muestra todos los datos
-        //dd($posts->find(3));
-        //dd($posts->findOneBy(['title'=> 'Welcome to US!']));
-
-        /* $microPost = new MicroPost();
-        $microPost->setTitle('It comes from controller');
-        $microPost->setText('Hi!');
-        $microPost->setCreated(new DateTime()); */
-        /* $microPost = $posts->find(3);
-        $posts->remove($microPost, true); */
-        
+    {     
         return $this->render('micro_post/index.html.twig', [
             'posts' => $posts->findAll(),
         ]);
@@ -39,7 +29,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('micro-post/add', name: 'app_micro_post_add', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $form = $this->createFormBuilder($microPost)
@@ -48,6 +38,19 @@ class MicroPostController extends AbstractController
          ->add('submit', SubmitType:: class, ['label'=>'Save'])
          ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->save($post, true);
+
+            //Add a flash
+            $this->addFlash('success', 'Your micro post have been add');
+           
+            return $this->redirectToRoute('app_micro_post');
+            //Redirect
+        }
         return $this->renderForm(
             'micro_post/add.html.twig',
             [
