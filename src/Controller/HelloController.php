b7feff0fdcc5a4127b01e3b2c75d60a57cd3bb00
Cristina Nicol Raipán Campos <cristina.raipan@globalconexus.com>
2023-07-26 17:53:48 -0400

    esplicacion de como se maneja la relacion uno a uno

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
@@ -13,12 +16,22 @@ class HelloController extends AbstractController
         ['message' => 'Bye!', 'created' => '2022/05/12']
     ];
 
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
-    public function index(int $limit): Response 
+    #[Route('/', name: 'app_index')]
+    public function index(UserProfileRepository $profiles): Response 
     {
+       /*  $user = new User();
+        $user->setEmail('email@email.com');
+        $user->setPassword('123');
+
+        $profile = new UserProfile();
+        $profile->setUser($user);
+        $profiles->save($profile, true); */
+
+        /* $profile= $profiles->find(1);
+        $profiles->remove($profile, true); */
         return $this->render ('hello/index.html.twig',
             ['messages' => $this-> messages, 
-             'limit' => $limit           
+             'limit' => 3         
             ]
         );
     }
