6e267f89180b41f6be89b5fd0fc6b7ad0a87991f
Cristina Nicol Raipán Campos <cristina.raipan@globalconexus.com>
2023-08-11 12:13:20 -0400

    manejo de permisos de los roles

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -9,6 +9,7 @@ use App\Form\CommentType;
 use App\Form\MicroPostType;
 use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,6 +26,7 @@ class MicroPostController extends AbstractController
         ]);
     }
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post): Response
     {
         return $this->render('micro_post/show.html.twig', [
@@ -33,16 +35,16 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
-       
         $form = $this->createForm(MicroPostType::class, new MicroPost());
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
             $post = $form->getData();
-            $post->setCreated(new DateTime());
+            $post->setAuthor($this->getUser());
             $posts->add($post, true);
 
             //Add a flash
@@ -59,10 +61,13 @@ class MicroPostController extends AbstractController
             );
     }
     #[Route('micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    #[IsGranted(MicroPost::EDIT, 'post')]
     public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
     {
         $form = $this->createForm(MicroPostType::class, $post);
         $form->handleRequest($request);
+
+        $this->denyAccessUnlessGranted(MicroPost::EDIT, $post);
 
         if ($form->isSubmitted() && $form->isValid()){
             $post = $form->getData();
@@ -83,6 +88,7 @@ class MicroPostController extends AbstractController
             );
     }
     #[Route('micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    #[IsGranted('ROLE_COMMENTER')]
     public function addComment(MicroPost $post, Request $request, CommentRepository $comments): Response
     {
         $form = $this->createForm(CommentType::class, new Comment());
@@ -91,6 +97,7 @@ class MicroPostController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()){
             $comment = $form->getData();
             $comment ->setPost($post);
+            $comment->setAuthor($this->getUser());
             $comments->add($comment, true);
 
             //Add a flash
